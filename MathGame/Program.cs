1781d57799fd0690696335daeb466efc9ebc19a4
vitmonjo <vitmonjo@gmail.com>
2024-08-21 20:39:08 -0300

    use a single method for all operations

--- MathGame/Program.cs
+++ MathGame/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Diagnostics;
+using System.Linq.Expressions;
 
 string? readResult;
 string menuSelection = "";
@@ -30,29 +31,19 @@ do
     switch (menuSelection)
     {
         case "1":
-            DefineDifficulty();
-            DefineNumOfQuestions();
-            RunAddGame();
+            RunGame('+');
             break;
         case "2":
-            DefineDifficulty();
-            DefineNumOfQuestions();
-            RunSubtractGame();
+            RunGame('-');
             break;
         case "3":
-            DefineDifficulty();
-            DefineNumOfQuestions();
-            RunMultiplyGame();
+            RunGame('*');
             break;
         case "4":
-            DefineDifficulty();
-            DefineNumOfQuestions();
-            RunDivideGame();
+            RunGame('/');
             break;
         case "5":
-            DefineDifficulty();
-            DefineNumOfQuestions();
-            RunRandomGame();
+            RunGame('!');
             break;
         case "6":
             PrintGames();
@@ -68,210 +59,20 @@ do
     }
 } while (menuSelection != "exit");
 
-void RunAddGame()
+void RunGame(char operation)
 {
+    SetupParameters();
+
     Stopwatch sw = new Stopwatch();
     sw.Start();
-
     bool allCorrect = true;
-    int i;
+    bool random = false;
+    int i = 0;
 
-    Console.WriteLine("Add Game");
+    if (operation == '!')
+        random = true;
 
-    for (i = 0; i < numOfQuestions; i++)
-    {
-        int num1 = GetRandomNumber();
-        int num2 = GetRandomNumber();
-        int num3;
-        
-        Console.WriteLine($"What is the result of {num1} + {num2}?");
-
-        var input = Console.ReadLine();
-        num3 = Convert.ToInt32(input);
-        if (num3 == (num1 + num2))
-        {
-            Console.WriteLine("Correct!");
-            Console.WriteLine("");
-        } else
-        {
-            Console.WriteLine("Not quite, try again next time!");
-            allCorrect = false;
-            break;
-        }
-    }
-
-    Console.WriteLine($"You've got {i} correct answer(s)!");
-
-    if (allCorrect)
-    {
-        Console.WriteLine("You've beaten the game, such a genius!");
-    }
-
-    sw.Stop();
-    Console.WriteLine($"Time taken: {sw.Elapsed.Seconds}s");
-    RegisterGame("Add", i.ToString(), sw.Elapsed.Seconds.ToString());
-
-    Console.ReadLine();
-    Console.Clear();
-}
-
-void RunSubtractGame()
-{
-    Stopwatch sw = new Stopwatch();
-    sw.Start();
-
-    bool allCorrect = true;
-    int i;
-
-    Console.WriteLine("Subtract Game");
-
-    for (i = 0; i < numOfQuestions; i++)
-    {
-        int num1 = GetRandomNumber();
-        int num2 = GetRandomNumber();
-        int num3;
-
-        Console.WriteLine($"What is the result of {num1} - {num2}?");
-
-        var input = Console.ReadLine();
-        num3 = Convert.ToInt32(input);
-        if (num3 == (num1 - num2))
-        {
-            Console.WriteLine("Correct!");
-            Console.WriteLine("");
-        }
-        else
-        {
-            Console.WriteLine("Not quite, try again next time!");
-            allCorrect = false;
-            break;
-        }
-    }
-
-    Console.WriteLine($"You've got {i} correct answer(s)!");
-
-    if (allCorrect)
-    {
-        Console.WriteLine("You've beaten the game, such a genius!");
-    }
-
-    sw.Stop();
-    Console.WriteLine($"Time taken: {sw.Elapsed.Seconds}s");
-    RegisterGame("Subtract", i.ToString(), sw.Elapsed.Seconds.ToString());
-
-    Console.ReadLine();
-    Console.Clear();
-}
-
-void RunMultiplyGame()
-{
-    Stopwatch sw = new Stopwatch();
-    sw.Start();
-
-    bool allCorrect = true;
-    int i;
-
-    Console.WriteLine("Multiply Game");
-
-    for (i = 0; i < numOfQuestions; i++)
-    {
-        int num1 = GetRandomNumber();
-        int num2 = GetRandomNumber();
-        int num3;
-
-        Console.WriteLine($"What is the result of {num1} * {num2}?");
-
-        var input = Console.ReadLine();
-        num3 = Convert.ToInt32(input);
-        if (num3 == (num1 * num2))
-        {
-            Console.WriteLine("Correct!");
-            Console.WriteLine("");
-        }
-        else
-        {
-            Console.WriteLine("Not quite, try again next time!");
-            allCorrect = false;
-            break;
-        }
-    }
-
-    Console.WriteLine($"You've got {i} correct answer(s)!");
-
-    if (allCorrect)
-    {
-        Console.WriteLine("You've beaten the game, such a genius!");
-    }
-
-    sw.Stop();
-    Console.WriteLine($"Time taken: {sw.Elapsed.Seconds}s");
-    RegisterGame("Multiply", i.ToString(), sw.Elapsed.Seconds.ToString());
-
-    Console.ReadLine();
-    Console.Clear();
-}
-
-void RunDivideGame()
-{
-    Stopwatch sw = new Stopwatch();
-    sw.Start();
-
-    int i;
-    bool allCorrect = true;
-
-    Console.WriteLine("Divide Game");
-
-    for (i = 0; i < numOfQuestions; i++)
-    {
-        int num1;
-        int num2;
-        int num3;
-
-        do
-        {
-            num1 = GetRandomNumber();
-            num2 = GetRandomNumber();
-        } while (num1 % 2 != 0 || num1 < num2 || num1 % num2 != 0);
-
-        Console.WriteLine($"What is the result of {num1} / {num2}?");
-
-        var input = Console.ReadLine();
-        num3 = Convert.ToInt32(input);
-        if (num3 == (num1 / num2))
-        {
-            Console.WriteLine("Correct!");
-            Console.WriteLine("");
-        }
-        else
-        {
-            Console.WriteLine("Not quite, try again next time!");
-            allCorrect = false;
-            break;
-        }
-    }
-
-    if (allCorrect)
-    {
-        Console.WriteLine("You've beaten the game, such a genius!");
-    }
-
-    sw.Stop();
-    Console.WriteLine($"Time taken: {sw.Elapsed.Seconds}s");
-    RegisterGame("Divide", i.ToString(), sw.Elapsed.Seconds.ToString());
-
-    Console.ReadLine();
-    Console.Clear();
-}
-
-void RunRandomGame()
-{
-    Stopwatch sw = new Stopwatch();
-    sw.Start();
-
-    int i;
-    bool allCorrect = true;
-
-    Console.WriteLine("Random Game");
+    Console.WriteLine($"{GetNameOfGame(operation, random)} Game");
 
     for (i = 0; i < numOfQuestions; i++)
     {
@@ -279,11 +80,11 @@ void RunRandomGame()
         int num2;
         int num3;
         int num4;
-        char operation;
 
-        do
-        {
+        if (random) 
             operation = GetRandomOperation();
+        do
+        {   
             num1 = GetRandomNumber();
             num2 = GetRandomNumber();
         } while (num1 % 2 != 0 || num1 < num2 || num1 % num2 != 0);
@@ -315,6 +116,8 @@ void RunRandomGame()
         }
     }
 
+    Console.WriteLine($"You've got {i} correct answer(s)!");
+
     if (allCorrect)
     {
         Console.WriteLine("You've beaten the game, such a genius!");
@@ -322,33 +125,37 @@ void RunRandomGame()
 
     sw.Stop();
     Console.WriteLine($"Time taken: {sw.Elapsed.Seconds}s");
-    RegisterGame("Random", i.ToString(), sw.Elapsed.Seconds.ToString());
+    RegisterGame(GetNameOfGame(operation, random), i.ToString(), sw.Elapsed.Seconds.ToString());
 
     Console.ReadLine();
     Console.Clear();
+
 }
 
-void PrintGames()
+void SetupParameters()
 {
+    DefineNumOfQuestions();
+    DefineDifficulty();
+}
 
-    Console.Clear();
-    Console.WriteLine("Previous Games");
+string GetNameOfGame(char operation, bool random)
+{
+    if (random)
+        return "Random";
 
-    for (int i = 0; i < games.GetLength(0); i++)
+    switch (operation)
     {
-        if (games[i, 0] != null)
-        {
-            Console.WriteLine($"Game {i + 1}");
-            Console.WriteLine($"Operation: {games[i, 0]}");
-            Console.WriteLine($"Num. of Correct Answers: {games[i, 1]}");
-            Console.WriteLine($"Time Taken: {games[i, 2]}s");
-            Console.WriteLine();
-        }
+        case '+':
+            return "Adding";
+        case '-':
+            return "Subtracting";
+        case '*':
+            return "Multiplying";
+        case '/':
+            return "Dividing";
+        default:
+            return "Invalid operation";
     }
-
-    Console.ReadLine();
-    Console.Clear();
-
 }
 
 int GetRandomNumber()
@@ -382,6 +189,29 @@ void PrintRules()
     Console.WriteLine("Don't forget to have a lot of fun!");
 
     Console.ReadLine();
+}
+
+void PrintGames()
+{
+
+    Console.Clear();
+    Console.WriteLine("Previous Games");
+
+    for (int i = 0; i < games.GetLength(0); i++)
+    {
+        if (games[i, 0] != null)
+        {
+            Console.WriteLine($"Game {i + 1}");
+            Console.WriteLine($"Operation: {games[i, 0]}");
+            Console.WriteLine($"Num. of Correct Answers: {games[i, 1]}");
+            Console.WriteLine($"Time Taken: {games[i, 2]}s");
+            Console.WriteLine();
+        }
+    }
+
+    Console.ReadLine();
+    Console.Clear();
+
 }
 
 void RegisterGame(string operation, string correctAnswers, string seconds)
